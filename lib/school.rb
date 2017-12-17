ce2197f627336b74eb88584ac93c6d92d38ffdef
brianabaker <brianaclairbaker@gmail.com>
2017-12-17 16:50:41 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+
+require 'pry'
+class School
+
+  attr_accessor :name, :roster
+
+  attr_reader :grade
+
+  def initialize(name)
+    @name = name
+    @roster= {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    # binding.pry
+    @roster.each do |grade, name_array|
+      name_array.sort!
+    end
+  end
+
+
+end
